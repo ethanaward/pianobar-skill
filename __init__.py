9fb94cc178e81dda0b30ef04a85c13b52a516862
Ethan Ward <ethan.ward@mycroft.ai>
2017-01-10 17:04:59 -0600

    Revert to normal keywords

--- __init__.py
+++ __init__.py
@@ -25,14 +25,13 @@ import subprocess
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
-from mycroft.skills.media import MediaSkill
 
 __author__ = 'eward'
 
 LOGGER = getLogger(__name__)
 
 
-class PianobarSkill(MediaSkill):
+class PianobarSkill(MycroftSkill):
 
     def __init__(self):
         super(PianobarSkill, self).__init__(name="PianobarSkill")
@@ -40,7 +39,6 @@ class PianobarSkill(MediaSkill):
 
     def initialize(self):
         self.load_data_files(dirname(__file__))
-        super(PianobarSkill, self).initialize()
 
         play_pandora_intent = IntentBuilder("PlayPandoraIntent").\
             require("PlayKeyword").require("PandoraKeyword").build()
