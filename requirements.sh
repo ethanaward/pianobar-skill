e64a69141d88cee89484e8e24e7ea6ce623b0de9
Michael Nguyen <ppnguyen91@gmail.com>
2017-10-20 18:12:37 -0500

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -5,6 +5,6 @@ found_exe() {
 }
 # polkit uses pkcon instead of apt-get
 # pkcon runs apt-get in the background
-if found_exe apt-get; then
+if found_exe pkcon; then
     pkcon install pianobar -y
 fi
