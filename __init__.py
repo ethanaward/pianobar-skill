869cdcba956b06c280bb0afdb3a83dd36b9a2b6f
Michael Nguyen <ppnguyen91@gmail.com>
2018-03-01 10:14:26 -0600

    only allow next song if pianobar is playing

--- __init__.py
+++ __init__.py
@@ -415,7 +415,7 @@ class PianobarSkill(MycroftSkill):
             self.speak_dialog("please.register.pandora")
 
     def handle_next_song(self, message=None):
-        if self.process:
+        if self.process and self.piano_bar_state == "playing":
             self.enclosure.mouth_think()
             self.process.stdin.write("n")
             self.piano_bar_state = "playing"
